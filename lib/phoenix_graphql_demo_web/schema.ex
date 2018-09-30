91223248e36164eba265ba52af331b2f010bd695
toda <starfish719@gmail.com>
2018-09-30 11:31:31 +0900

    use pipe

--- lib/phoenix_graphql_demo_web/schema.ex
+++ lib/phoenix_graphql_demo_web/schema.ex
@@ -18,7 +18,7 @@ defmodule PhoenixGraphqlDemoWeb.Schema do
       arg :id, non_null(:id)
       resolve &PokemonResolver.pokemon/3
     end
-    field :all_pokemons, non_null(list_of(non_null(:pokemon))) do
+    field :all_pokemons, :pokemon|>non_null|>list_of|>non_null do
       resolve &PokemonResolver.all_pokemons/3
     end
 
@@ -26,7 +26,7 @@ defmodule PhoenixGraphqlDemoWeb.Schema do
       arg :id, non_null(:id)
       resolve &PokemonResolver.type/3
     end
-    field :all_types, non_null(list_of(non_null(:type))) do
+    field :all_types, :type|>non_null|>list_of|>non_null do
       resolve &PokemonResolver.all_types/3
     end
   end
